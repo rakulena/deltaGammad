3b4c1714ca07967ca52897ec6970955519c572d2
zhanghy <1586511364@qq.com>
2026-04-01 11:51:05 +0800

    Add files via upload

---
+++ zhengli/kst_ks/hist/input/sele_optdef.h
@@ -0,0 +1,13 @@
+
+// 'sele' for the tree in selection study
+//kstar
+    //  char sele_tree1[] = "~/Sample/data/Bd2JpsiKstar/presel_Bd2JpsiKstar_data_\%i_sw_base.root/DecayTree";
+    //  char sele_tree3[] = "~/Sample/mc/Bd2JpsiKstar/Bd2JpsiKstar_\%i_MC_selected_fewvars_presel_gbw.root/DecayTree";
+    //  char sele_tree2[] = "~/Sample/data/Bd2JpsiKstar/Bd2JpsiKstar_\%i_data_selected_fewvars_presel.root/DecayTree";
+
+
+//kshort
+     char sele_tree1[] = "~/Sample/data/Bd2JpsiKshort/presel_Bd2JpsiKshort_data_\%i_sw_base.root/DecayTree";
+     char sele_tree3[] = "~/Sample/mc/Bd2JpsiKshort/Bd2JpsiKshort_\%i_MC_selected_fewvars_presel_gbw.root/DecayTree";
+     char sele_tree2[] = "~/Sample/data/Bd2JpsiKshort/Bd2JpsiKshort_\%i_data_selected_fewvars_presel.root/DecayTree";
+
